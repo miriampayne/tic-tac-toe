9a5c16ac9572e3cef0afb183342036a9dd0bbc0d
Miriam Payne <miriampayne@gmail.com>
2022-04-10 02:32:19 +0000

    Feat: add result validation and restart functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
 /*
 Game status element
 */
-const statusDisplay = document.getElementsByClass('game-status');
+const statusDisplay = document.querySelector('.game--status');
 /*
 Variables for tracking game state throughout
-*/
-/*
 Pause game in case of end scenario
-*/
-let gameActive = true;
-/*
 Store current player, so we know whos turn 
-*/
-let currentPlayer = "X";
-/*
 Store current game state here as empty strings in an array
 to track played cells and validate game state later on
 */
+let gameActive = true;
+let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
 /*
 Messages will display to user during game.
@@ -30,44 +24,101 @@ const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 Set inital message to let players know whose turn it is
 */
 statusDisplay.innerHTML = currentPlayerTurn();
-function handleCellPlayed() {
 
-}
-function handlePlayerChange() {
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-}
-function handleResultValidation() {
-
-}
-function handleCellClick() {
+function handleCellPlayed(clickedCell, clickedCellIndex) {
     /*
-Save clicked element in constant var
-*/    
-    const clickedCell = clickedCellEvent.target;
-/*
-Get 'data-cell-index' attribute from clicked cell to identify location on grid. 
-getAttribute returns string => parse to integer
-*/
-    const i = parseInt(
-      clickedCell.getAttribute('cell-i')
-    );
-/* 
-Check if cell already played or if game paused and if either true, ignore click.
-*/
-    if (gameState[i] !== "" || !gameActive) {
+    Update internal game state and UI to reflect played move
+    */
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
         return;
     }
-/* 
-If everything if in order we will proceed with the game flow
-*/    
-    handleCellPlayed(clickedCell, i);
+    /* 
+    Check if any values in game state array 
+    that are still not populated with a player sign
+    */
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+    /*
+    If here, no one won game yet, still moves to be played,
+    continue by changing the current player.
+    */
+    handlePlayerChange();
+}
+
+function handleCellClick(clickedCellEvent) {
+    /*
+    Save clicked element in constant var
+    */    
+    const clickedCell = clickedCellEvent.target;
+    /*
+    Get 'data-cell-index' attribute from clicked cell to identify location on grid. 
+    getAttribute returns string => parse to integer
+    */
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+    /* 
+    Check if cell already played or if game paused and if either true, ignore click.
+    */
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+    /* 
+    If everything if in order we will proceed with the game flow
+    */
+    handleCellPlayed(clickedCell, clickedCellIndex);
     handleResultValidation();
 }
-function handleRestartGame() {
 
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
 }
 /*
 Add event listeners to game cells and restart button
 */
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.getElementById('restart-btn').addEventListener('click', handleRestartGame);
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
